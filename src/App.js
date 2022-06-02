ba2e3813780f07e0bf89e38b5c60a35a31aba0c3
iqbalaulia <iqbal.developer16@gmail.com>
2022-06-02 18:30:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="Parentbox">
+      <ProductImage />
+      <ProductInfo name="AJ Maximum" category="TAHUN BARU" />
+    </div>
+  );
+}
+
+function ProductImage() {
+  return (
+    <div className="Foto">
+      <img alt="foto sepatu" src="/assets/img/sepatuCover.jpg" />
+    </div>
+  );
+}
+
+function ProductInfo(props) {
+  const { category, name } = props;
+  const price = 750000;
+  return (
+    <div>
+      <div className="Deskripsi">
+        <p className="Cate">{category}</p>
+        <h1 className="Title"> {name}</h1>
+        <p className="Price">IDR {price}</p>
+        <p className="Info">
+          One of the most recognizable shoes in the IQ collection, the Air
+          Jordan 5 Maximus features lightweight, visible cushioning just like
+          the original from '88. Signature details and materials celebrate the
+          game-changing icon.'
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
